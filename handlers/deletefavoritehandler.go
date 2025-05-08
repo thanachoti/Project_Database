d2994e973f66377996f356be63d9362c56657e59
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-05-08 21:29:33 +0700

    handling favorite id and remove logic

--- handlers/deletefavoritehandler.go
+++ handlers/deletefavoritehandler.go
@@ -6,14 +6,19 @@ import (
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/khemingkapat/been_chillin/auth"
 )
 
 // DeleteFavoriteHandler deletes a favorite by id
 func DeleteFavoriteHandler(db *sql.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		id := c.Params("user_id")
+		contentID := c.Params("content_id")
+		userID, err := auth.ExtractUserID(c)
+		if err != nil {
+			return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error": "you are not logged in"})
+		}
 
-		res, err := db.Exec(`DELETE FROM FAVORITE WHERE favorite_id = $1`, id)
+		res, err := db.Exec(`DELETE FROM FAVORITE WHERE user_id = $1 and content_id = $2`, userID, contentID)
 		if err != nil {
 			log.Println("Delete error:", err)
 			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "failed to delete favorite"})
