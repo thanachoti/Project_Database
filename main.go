838e9550366b2ed17c7d1956456bd9e292eb76b5
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-05-09 17:33:53 +0700

    change cors

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5174",
+		AllowOrigins:     "http://localhost:5173",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS,PATCH",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
